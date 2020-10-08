1e114465654f9700e05ab2e8e6d6738777d240f1
annie <ruthannie3@gmail.com>
2020-10-08 11:40:37 +0530

    [Annie]Added: Method to find maximum among three integers

--- src/main/java/com/bridgelabz/test/testmaximum/FindMaximum.java
+++ src/main/java/com/bridgelabz/test/testmaximum/FindMaximum.java
@@ -1,9 +1,20 @@
 package com.bridgelabz.test.testmaximum;
 
 public class FindMaximum {
-
+	// Welcome message
 	public void welcomeMessage() {
 		System.out.println("Welcome to Test Maximum of 3 variables");
 	}
 
+	// Find maximum among three integers
+	public int maximumAmongThreeIntegers(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (y.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
 }
--- src/test/java/com/bridgelabz/test/testmaximum/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/test/testmaximum/FindMaximumTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.test.testmaximum;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,5 +15,16 @@ public class FindMaximumTest {
 	@Test
 	public void welcome() {
 		findMaximum.welcomeMessage();
+	}
+
+	@Test
+	public void givenIntegers_whenFindMax_returnTrue() {
+		int maximumInt = findMaximum.maximumAmongThreeIntegers(5, 8, 2);
+		Assert.assertEquals(8, maximumInt);
+	}
+	@Test
+	public void givenIntegers_whenNotFindMax_returnFalse() {
+		int maximumInt = findMaximum.maximumAmongThreeIntegers(10, 8, 12);
+		Assert.assertNotEquals(8, maximumInt);
 	}
 }
