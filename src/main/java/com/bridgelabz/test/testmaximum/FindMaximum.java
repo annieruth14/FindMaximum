208e8636ab234703b33d7f2a40bd25aa6d24efe4
annie <ruthannie3@gmail.com>
2020-10-08 12:17:01 +0530

    [Annie]Added: method to find maximum among three floats

--- src/main/java/com/bridgelabz/test/testmaximum/FindMaximum.java
+++ src/main/java/com/bridgelabz/test/testmaximum/FindMaximum.java
@@ -1,18 +1,27 @@
 package com.bridgelabz.test.testmaximum;
 
 public class FindMaximum {
-	// Welcome message
+	// Welcome message 
 	public void welcomeMessage() {
 		System.out.println("Welcome to Test Maximum of 3 variables");
 	}
-
 	// Find maximum among three integers
 	public int maximumAmongThreeIntegers(Integer x, Integer y, Integer z) {
 		Integer max = x;
 		if (y.compareTo(max) > 0) {
 			max = y;
 		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
+	public float maximumAmongThreeFloat(Float x, Float y, Float z) {
+		Float max = x;
 		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
 			max = z;
 		}
 		return max;
--- src/test/java/com/bridgelabz/test/testmaximum/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/test/testmaximum/FindMaximumTest.java
@@ -19,16 +19,34 @@ public class FindMaximumTest {
 	@Test
 	public void givenIntegers_whenFindMaxAtPosition1_returnTrue() {
 		int maximumInt = findMaximum.maximumAmongThreeIntegers(8, 5, 7);
-		Assert.assertEquals(8, maximumInt);
+		Assert.assertEquals(maximumInt, 8);
 	}
 	@Test
 	public void givenIntegers_whenFindMaxAtPosition2_returnTrue() {
 		int maximumInt = findMaximum.maximumAmongThreeIntegers(5, 8, 2);
-		Assert.assertEquals(8, maximumInt);
+		Assert.assertEquals(maximumInt, 8);
 	}
 	@Test
 	public void givenIntegers_whenFindMaxAtPosition3_returnTrue() {
 		int maximumInt = findMaximum.maximumAmongThreeIntegers(10, 8, 12);
-		Assert.assertEquals(12, maximumInt);
+		Assert.assertEquals(maximumInt , 12);
+	}
+
+	@Test
+	public void givenFloats_whenFindMaxAtPosition1_returnTrue() {
+		float maximumFloat = findMaximum.maximumAmongThreeFloat(8.5f, 5.8f, 7.8f);
+		Assert.assertEquals(maximumFloat, 8.5f, 0.0f);
+		//(8, maximumFloat);
+	}
+	@Test
+	public void givenFloats_whenFindMaxAtPosition2_returnTrue() {
+		float maximumFloat = findMaximum.maximumAmongThreeFloat(5.3f, 8.6f, 2.4f);
+		Assert.assertEquals(maximumFloat, 8.6f, 0.0f);
+	}
+	@Test
+	public void givenFloats_whenFindMaxAtPosition3_returnTrue() {
+		float maximumFloat = findMaximum.maximumAmongThreeFloat(10.9f, 8.7f, 12.4f);
+		System.out.println(maximumFloat);
+		Assert.assertEquals(maximumFloat, 12.4f, 0.0f);
 	}
 }
